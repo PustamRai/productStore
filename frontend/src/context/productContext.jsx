c02ba0347f61e762a790595f4a5032412191554f
Pustam Rai <github.pustam@gmail.com>
2025-03-12 22:14:06 +0545

    fetch data from backend to frontend

--- frontend/src/context/productContext.jsx
+++ frontend/src/context/productContext.jsx
@@ -1,21 +1,41 @@
-import React, { createContext, useState, useContext } from 'react';
-
+import React, { createContext, useState, useContext, useEffect } from "react";
+import { API } from "../api/API";
+import { toast } from "react-hot-toast"
 
 const ProductContext = createContext();
 
 // create provider
-export const ProductProvider = ({children}) => {
-    const [products, setProducts] = useState([])
+export const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
 
-    const addProduct = (products) => {
-        setProducts((prevProducts) => [...prevProducts, products])
+  // fetch products from backend
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await API.get("/allProducts");
+        const allProducts = response.data.data
+
+        setProducts((prevProducts) => [...prevProducts, allProducts])
+        toast.success(response.data.message);
+      } catch (error) {
+        toast.error("error in fetching data");
+      }
     }
 
-    return (
-        <ProductContext.Provider value={{products, addProduct}}>
-            {children}
-        </ProductContext.Provider>
-    )
-}
+    fetchProducts()
+  }, []);
 
-export const useProductContext = () => useContext(ProductContext)
+
+  // function to add a new product
+  const addProduct = (products) => {
+    setProducts((prevProducts) => [...prevProducts, products]);
+  };
+
+  return (
+    <ProductContext.Provider value={{ products, addProduct }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+export const useProductContext = () => useContext(ProductContext);
