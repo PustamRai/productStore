25ad72523008a20a60850780b217b654a611af89
Pustam Rai <github.pustam@gmail.com>
2025-03-12 18:35:27 +0545

    add context api

---
+++ frontend/src/context/productContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext, useState } from "react";
+
+const ProductContext = createContext();
+
+// create provider
+export const ProductProvider = ({children}) => {
+    const [products, setProducts] = useState([])
+
+    const addProduct = (products) => {
+        setProducts((prevProducts) => [...prevProducts, products])
+    }
+
+    return (
+        <ProductContext.Provider value={{products, addProduct}}>
+            {children}
+        </ProductContext.Provider>
+    )
+}
+
+export const useProductContext = () => useContext(ProductContext)
