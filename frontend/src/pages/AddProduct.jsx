cc583a3fa612e51ab1acdfdc2a5fa7ee989ad4d6
Pustam Rai <github.pustam@gmail.com>
2025-03-15 15:56:46 +0545

    add form data to manage image upload

--- frontend/src/pages/AddProduct.jsx
+++ frontend/src/pages/AddProduct.jsx
@@ -25,23 +25,28 @@ function AddProduct() {
     formData.append("price", price);
     formData.append("image", imageFile);
 
+    console.log("form data: ", formData)
+
     try {
       const response = await API.post("/addProduct", formData, {
         headers: { "Content-Type": "multipart/form-data" }, 
       });
+
+      console.log("res: ", response)
 
       const newProduct = response.data.data;
       addProduct(newProduct);
       toast.success(response.data.message);
       navigate("/");
     } catch (error) {
-      toast.error("failed to add product");
+      // toast.error("failed to add product");
+      toast.error(error.response?.data?.message || "Failed to add product");
     }
   };
 
   return (
     <div className="flex justify-center min-h-screen bg-gray-900">
-      <div className="bg-gray-900 py-15 rounded-lg shadow-lg w-80 md:w-2xl">
+      <div className="bg-gray-900 px-6 py-15 rounded-lg shadow-lg w-2xl">
         <h2 className="text-white text-lg font-semibold text-center mb-4">
           Create New Product
         </h2>
@@ -70,14 +75,14 @@ function AddProduct() {
           />
           <input
             type="file"
-            name="imageUrl"
+            name="imageFile"
             onChange={(e) => setImageFile(e.target.files[0])} 
             className="w-full p-2 mb-3 bg-gray-700 text-white rounded cursor-pointer"
             required
           />
           <button
             type="submit"
-            className="w-full pt-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition"
+            className="w-full pt-2 bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition cursor-pointer"
           >
             Add Product
           </button>
